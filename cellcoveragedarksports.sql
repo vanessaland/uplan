a8d058f90b359b220fa3db170a0fb4969945c329
Vanessa Landayan <landayan.v@gmail.com>
2021-01-09 12:51:20 -0500

    query for cell coverage dark spots

---
+++ cellcoveragedarksports.sql
@@ -0,0 +1,9 @@
+/* This query is used for determining the cell coverage dark spots in Canada. */
+
+SELECT 
+	GeohashBounds
+    , Country
+	, AvgDropsPerMonth
+FROM `geotab-public-intelligence.UrbanInfrastructure.CellCoverageDarkSpots`
+WHERE Country LIKE 'Canada'
+AND AvgDropsPerMonth BETWEEN 0.009 AND 0.01
