c2675dd02b171f90fe4111209bdba2fb2462f396
Vanessa Landayan <landayan.v@gmail.com>
2021-01-09 13:08:43 -0500

    query for intersection metrics in waterloo

---
+++ intersectionmetrics.sql
@@ -0,0 +1,14 @@
+/* This query is used for determining the wait times at Intersections in Waterloo, Ontario, Canada. */
+
+SELECT 
+  GeogPoint
+  , PercentageStopping
+  , TheDate
+  , City
+  , Country
+  , StreetNames
+  
+FROM `geotab-public-intelligence.UrbanInfrastructure.IntersectionMetrics`
+
+WHERE City LIKE 'Waterloo'
+AND Country LIKE 'Canada'
